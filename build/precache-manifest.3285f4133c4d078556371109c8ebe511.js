d7f29322dabae27212a8c50dfb500d9891affb32
Enzo Albuquerque <enzosalbu@gmail.com>
2024-03-31 16:09:46 -0300

    Procfile

---
+++ build/precache-manifest.3285f4133c4d078556371109c8ebe511.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f28bb798be321803e8e68e942a3ad5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94dea4b6dcee561bb662",
+    "url": "./static/css/2.df6660d7.chunk.css"
+  },
+  {
+    "revision": "da37faf2ad8eecbe5c36",
+    "url": "./static/css/main.0c7808b4.chunk.css"
+  },
+  {
+    "revision": "94dea4b6dcee561bb662",
+    "url": "./static/js/2.c47a8011.chunk.js"
+  },
+  {
+    "revision": "da37faf2ad8eecbe5c36",
+    "url": "./static/js/main.386de0d1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1f7ca6383ea7c74a7f5ddd76c3d3cef2",
+    "url": "./static/media/Inter-italic.var.1f7ca638.woff2"
+  },
+  {
+    "revision": "66c6e40883646a7ad993108b2ce2da32",
+    "url": "./static/media/Inter-roman.var.66c6e408.woff2"
+  },
+  {
+    "revision": "d5a2c6b155b8c65866c9853c686464bf",
+    "url": "./static/media/logo.d5a2c6b1.png"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "./static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "./static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "./static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "./static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "./static/media/primeicons.c0929a1e.woff2"
+  }
+]);
