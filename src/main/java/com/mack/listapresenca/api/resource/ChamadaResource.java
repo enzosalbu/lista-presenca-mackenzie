e7ff132144d169bef8e3cf9a4a0b9b0ccecd6be2
Enzo Albuquerque <enzosalbu@gmail.com>
2024-03-31 01:34:05 -0300

    crossOrigin

--- src/main/java/com/mack/listapresenca/api/resource/ChamadaResource.java
+++ src/main/java/com/mack/listapresenca/api/resource/ChamadaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/chamadas")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ChamadaResource {
 	
 	private final ChamadaService service;
